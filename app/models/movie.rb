f5b167546c22f2967e3bdfcc1f78d7869e5352ed
Nitish313 <student.nitishanand@gmail.com>
2024-01-06 12:16:19 +0530

    Create movie model and serialize show_timings

---
+++ app/models/movie.rb
@@ -0,0 +1,3 @@
+class Movie < ApplicationRecord
+  serialize :show_timings, Array
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_05_075321) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_05_200359) do
+  create_table "movies", force: :cascade do |t|
+    t.string "title"
+    t.string "genre"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
